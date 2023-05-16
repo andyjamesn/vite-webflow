c01f0d2a4d88aa1da806c545520aa86a75026a38
allefts <alleftsoares@gmail.com>
2023-05-16 15:03:17 -0500

    scrolling progress and snap have been fixed, lets go

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const navButton = document.getElementById("nav-toggle");
   const scrollContainer = document.querySelector("main");
   const scrollProgress = document.getElementById("scroll-progress");
-  const docWidth =
-    document.documentElement.scrollWidth + document.documentElement.clientWidth;
+
+  console.log(document.documentElement.scrollWidth);
+  console.log(document.documentElement.clientWidth);
 
   const toggleNav = () => {
     document.body.dataset.nav =
@@ -19,15 +20,20 @@ window.addEventListener("DOMContentLoaded", () => {
     //Scroll Page
     let scrollBy = 0;
     if (e.deltaY > 0) {
-      scrollBy = e.deltaY + 400;
+      scrollBy = e.deltaY + 1000;
     } else {
-      scrollBy = e.deltaY - 400;
+      scrollBy = e.deltaY - 1000;
     }
+
     scrollContainer!.scrollLeft += scrollBy;
   });
 
   //Scroll Percentage
   scrollContainer!.addEventListener("scroll", () => {
+    const docWidth =
+      document.documentElement.scrollWidth +
+      document.documentElement.clientWidth;
+
     const scrollLeft = scrollContainer!.scrollLeft;
     scrollProgress!.style.width = `${(scrollLeft / docWidth) * 100}%`;
   });
