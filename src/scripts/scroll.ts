9f7e34e859cc231cee4995c612591fb3bdb88903
allefts <alleftsoares@gmail.com>
2023-05-29 17:14:16 -0500

    hopefully gsap is not too heavy

--- src/scripts/scroll.ts
+++ src/scripts/scroll.ts
@@ -1,8 +1,12 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
 const sections = gsap.utils.toArray(".page-container section");
+const docSections = document.querySelectorAll(".nav-link-label");
+const clientHeight = document.documentElement.clientHeight;
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
@@ -15,4 +19,13 @@ gsap.to(sections, {
     // snap: 1 / (sections.length - 1),
     // markers: true,
   },
+});
+
+docSections.forEach((sec, idx) => {
+  sec.addEventListener("click", () => {
+    gsap.to(window, {
+      duration: 2,
+      scrollTo: { y: clientHeight * idx, offsetY: -50 * idx },
+    });
+  });
 });
