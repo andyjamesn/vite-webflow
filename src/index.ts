e611b70a25f4d8054ffc3ed501b4c4a133e96293
allefts <alleftsoares@gmail.com>
2023-06-30 14:58:06 -0500

    still fixing phone issues, for some reason iphone 7 does not look good at all but on desktop narrow res it looks fine

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { setUpDarkMode } from "./scripts/darkMode";
 import "./scripts/gsap.ts";
 
 //Styles
-import "./styles/style.css";
-import "./styles/queries.css";
-import "./styles/utils.css";
-import "./styles/keyframes.css";
+// import "./styles/style.css";
+// import "./styles/queries.css";
+// import "./styles/utils.css";
+// import "./styles/keyframes.css";
 
 window.addEventListener("DOMContentLoaded", () => {
   const navButton = document.getElementById("nav-toggle");
